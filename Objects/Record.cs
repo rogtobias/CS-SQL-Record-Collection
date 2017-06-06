2e3aad47277ad3e9919dcaedecfb58a53de8ccfa
Jordan Mysliwiec and Roger Tobias <jordan+roger@>
2017-06-06 14:12:03 -0700

    add additional tests for DeleteAll(), Find(), and Save() methods. Add exported sql files for database.

--- Objects/Record.cs
+++ Objects/Record.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace RecordCollection
 {
-  public class RecordCollection
+  public class Record
   {
     private int _id;
     private string _artist;
@@ -13,6 +13,19 @@ namespace RecordCollection
     {
       _id = Id;
       _artist = Artist;
+    }
+    public override bool Equals(System.Object otherRecord)
+    {
+      if(!(otherRecord is Record))
+      {
+        return false;
+      }
+      else
+      {
+        Record newRecord = (Record) otherRecord;
+        bool artistEquality = (this.GetArtist() == newRecord.GetArtist());
+        return (artistEquality);
+      }
     }
 
     public int GetId()
@@ -23,7 +36,7 @@ namespace RecordCollection
     {
       return _artist;
     }
-    public void SetArtist()
+    public void SetArtist(string newArtist)
     {
       _artist = newArtist;
     }
@@ -54,6 +67,71 @@ namespace RecordCollection
         conn.Close();
       }
       return allRecords;
+    }
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO records (artist) OUTPUT INSERTED.id VALUES (@RecordArtist);", conn);
+
+      SqlParameter artistParameter = new SqlParameter();
+      artistParameter.ParameterName = "@RecordArtist";
+      artistParameter.Value = this.GetArtist();
+      cmd.Parameters.Add(artistParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM records;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+    public static Record Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM records WHERE id = @RecordId;", conn);
+      SqlParameter recordIdParameter = new SqlParameter();
+      recordIdParameter.ParameterName = "@RecordId";
+      recordIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(recordIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      int foundRecordId = 0;
+      string foundRecordArtist = null;
+      while(rdr.Read())
+      {
+        foundRecordId = rdr.GetInt32(0);
+        foundRecordArtist = rdr.GetString(1);
+      }
+      Record foundRecord = new Record(foundRecordArtist, foundRecordId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return foundRecord;
     }
   }
 }
--- Tests/RecordTest.cs
+++ Tests/RecordTest.cs
@@ -10,7 +10,7 @@ namespace RecordCollection
   {
     public RecordCollectionTest()
     {
-      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=record_collection_test;Integrated Security=SSPI;";
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=record_collection_tests;Integrated Security=SSPI;";
     }
     public void Dispose()
     {
@@ -26,5 +26,43 @@ namespace RecordCollection
       //Assert
       Assert.Equal(0, result);
     }
+
+    [Fact]
+    public void Test_Equal_ReturnsTrueIfArtistAreTheSame()
+    {
+      //Arrange, Act
+      Record firstRecord = new Record("Primus");
+      Record secondRecord = new Record("Primus");
+
+      //Assert
+      Assert.Equal(firstRecord, secondRecord);
+    }
+    [Fact]
+    public void Test_Save_SavesToDatabase()
+    {
+      //Arrange
+      Record testRecord = new Record("Primus");
+      //Act
+      testRecord.Save();
+      List<Record> result = Record.GetAll();
+      List<Record> testList = new List<Record>{testRecord};
+
+      //Assert
+      Assert.Equal(testList, result);
+    }
+    [Fact]
+    public void Test_Find_FindsRecordInDatabase()
+    {
+      //Arrange
+      Record testRecord = new Record("Primus");
+      testRecord.Save();
+
+      //Act
+      Re foundRecord = Record.Find(testRecord.GetId());
+
+      //Assert
+      Assert.Equal(testRecord, foundRecord);
+    }
+
   }
 }
